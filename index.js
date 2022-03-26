3f0fb56701ec22fded4548d0b4dbf68e20ce41cb
basit <basitng2004@gmail.com>
2022-03-26 16:17:22 -0700

    handled better error message handling in product.js

--- index.js
+++ index.js
@@ -3,21 +3,19 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cookieSession = require("cookie-session");
-var cors = require("cors");
-const bodyParser = require("body-parser");
-
-const router = require("./Routes/API/auth");
-const AllAPI = require("./Routes/API/endpoints");
 const app = express();
-
+var cors = require("cors");
 const corsOptions = {
   origin: "*",
   credentials: true,
   optionSuccessStatus: 200,
 };
+app.use(cors(corsOptions));
+const bodyParser = require("body-parser");
+const router = require("./Routes/API/auth");
+const AllAPI = require("./Routes/API/endpoints");
 
 // MIDDLEWARES
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(
   cookieSession({
