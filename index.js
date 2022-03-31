ef463df966c393add27733d3386f2e1b7060b0cc
basit <basitng2004@gmail.com>
2022-03-31 15:20:54 -0700

    added changes

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const cookieParser = require("cookie-parser");
 const cookieSession = require("cookie-session");
 const app = express();
 
-const bodyParser = require("body-parser");
-const router = require("./Routes/API/auth");
-const AllAPI = require("./Routes/API/endpoints");
-
 // MIDDLEWARES
 const corsOptions = {
   origin: "*",
@@ -35,6 +31,11 @@ app.use("/api", AllAPI);
 dotenv.config();
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 const PORT = process.env.PORT || 5000;
+
+const bodyParser = require("body-parser");
+const router = require("./Routes/API/auth");
+const AllAPI = require("./Routes/API/endpoints");
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
